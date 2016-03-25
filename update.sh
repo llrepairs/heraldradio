853bc3a5d218b4ae7a47d91dded76b5d7bda119d
llrepairs <help@llrepairs.com>
2016-03-25 11:38:16 +0000

    Update update.sh

--- update.sh
+++ update.sh
@@ -4,6 +4,10 @@ sudo cp heraldradio /etc/init.d/heraldradio
 sudo chmod ugo+x /etc/init.d/heraldradio
 sudo update-rc.d heraldradio defaults
 sudo chmod +x herald.sh
+sudo rm /etc/rc.local
+sudo cp rc.local /etc/rc.local
+sudo chown root /etc/rc.local
+sudo chmod 755 /etc/rc.local
 sudo apt-get install rpi-update
 sudo rpi-update
 sudo reboot
