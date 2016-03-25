1cfae40ef7d1b0bd225c45a5e90b7dcfeafc5eeb
llrepairs <help@llrepairs.com>
2016-03-25 09:42:52 +0000

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,7 +1,7 @@
 sudo apt-get install vlc
-sudo cp heraldstart /etc/init.d/heraldstart
-sudo chmod ugo+x /etc/init.d/heraldstart
-sudo update-rc.d heraldstart defaults
+sudo cp heraldradio /etc/init.d/heraldradio
+sudo chmod ugo+x /etc/init.d/heraldradio
+sudo update-rc.d heraldradio defaults
 sudo apt-get install rpi-update
 sudo rpi-update
 sudo reboot
